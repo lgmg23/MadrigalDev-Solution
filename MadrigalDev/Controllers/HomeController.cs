68845b1181a311dda55cb80691d04ff50d16fa8d
Luis Gerardo Madrigal <luisgerardo_mg@hotmail.com>
2018-08-19 20:22:20 -0500

    hub para soportar signal r

--- MadrigalDev/Controllers/HomeController.cs
+++ MadrigalDev/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MadrigalDev.Hubs;
+using MadrigalDev_DAL;
 
 namespace MadrigalDev.Controllers
 {
@@ -25,6 +27,28 @@ namespace MadrigalDev.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Data()
+        {
+            madrigaldev_testEntities data = new madrigaldev_testEntities();
+            var datos = data.t_empresa.ToList();
+            return PartialView("_DataList", datos);
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(t_empresa model)
+        {
+            madrigaldev_testEntities context = new madrigaldev_testEntities();
+            var added = context.t_empresa.Add(model);
+            context.SaveChanges();
+            CustomerHub.BroadcastData();
+            return View("Insert");
         }
     }
 }
